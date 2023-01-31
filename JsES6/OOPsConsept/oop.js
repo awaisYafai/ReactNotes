e21938cab4cb50633cf471fa9a517af2e0987a50
Awais Yafai <awaisyafai0079@gmail.com>
2023-01-31 17:48:23 +0530

    es6

---
+++ JsES6/OOPsConsept/oop.js
@@ -0,0 +1,19 @@
+// class and object
+
+// class is a blueprint of making objects.. 
+
+class customer {
+    constructor(n){
+        this.name = n;
+    }
+ // property
+
+buy(){
+    console.log('hello');
+} // methode
+}
+
+let customer1 = new customer('awais yafai') // veriable
+
+console.log(customer1);
+customer1.buy();
---
+++ JsES6/arrayDestructurig/destructuring.js
@@ -0,0 +1,8 @@
+// Array Destructuring
+
+// Array element or object property ko alag alag veriable me khud se store kra dena
+
+let list = ["audi", "bmw", "honda"];
+let [car1, car2, car3] = list;
+
+console.log(car1);
